dfe7612b6e671b6a1d783e08a8081143adf0424a
cgungaloo <cgungaloo@yahoo.com>
2018-03-21 16:42:29 +0000

    added driver in hub to be static fix

--- src/test/java/utils/SetProperty.java
+++ src/test/java/utils/SetProperty.java
@@ -23,14 +23,14 @@ public class SetProperty {
                 DesiredCapabilities dc = DesiredCapabilities.chrome();
                 dc.setBrowserName("chrome");
                 dc.setPlatform(Platform.XP);
-                WebDriver driver = new RemoteWebDriver(new URL("http://172.31.2.199:5566/wd/hub"),dc);
+                driver = new RemoteWebDriver(new URL("http://172.31.2.199:5566/wd/hub"),dc);
                 driver.get(GetConfig.configItems.get("web_address"));
 
             }else if(GetConfig.configItems.get("browser").equals("firefox")){
                 DesiredCapabilities dc = DesiredCapabilities.firefox();
                 dc.setBrowserName("firefox");
                 dc.setPlatform(Platform.XP);
-                WebDriver driver = new RemoteWebDriver(new URL("http://172.31.2.199:5566/wd/hub"),dc);
+                driver = new RemoteWebDriver(new URL("http://172.31.2.199:5566/wd/hub"),dc);
                 driver.get(GetConfig.configItems.get("web_address"));
             }
         }else{
