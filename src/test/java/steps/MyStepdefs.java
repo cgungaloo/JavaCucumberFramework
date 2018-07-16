b72712fcf287a229b99cf48385aa1ab46ed0f497
cgungaloo <cgungaloo@yahoo.com>
2018-07-16 18:19:22 +0100

    Changed steps to prints

--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -12,25 +12,29 @@ public class MyStepdefs {
     private Home homepage;
     @Given("^I am on the home page$")
     public void iAmOnTheHomePage() {
-       homepage = Page_Factory.getHomePage();
-       assertTrue(homepage.checkHomePage());
+//       homepage = Page_Factory.getHomePage();
+//       assertTrue(homepage.checkHomePage());
+        System.out.println("I am on the home page");
     }
 
     @When("^I enter the search term \"([^\"]*)\" into the search bar$")
     public void iEnterTheSearchTermIntoTheSearchBar(String arg0) throws Throwable {
-        homepage.SearchForItem(arg0);
-        homepage.SelectSearchByXpath();
+//        homepage.SearchForItem(arg0);
+//        homepage.SelectSearchByXpath();
+        System.out.println("I am on the search items");
     }
 
     @When("^I click on Sign in$")
     public void iClickOnSignIn() throws Throwable {
-        homepage = Page_Factory.getHomePage();
-        homepage.clickOnElementByXPath("//*[@id=\"gb_70\"]");
+//        homepage = Page_Factory.getHomePage();
+//        homepage.clickOnElementByXPath("//*[@id=\"gb_70\"]");
+        System.out.println("I am on the Sign in");
     }
 
     @When("^I click on terms$")
     public void iClickOnTerms() throws Throwable {
-        homepage.clickOnTerms();
+//        homepage.clickOnTerms();
+        System.out.println("I clicked on terms");
     }
 
 }
